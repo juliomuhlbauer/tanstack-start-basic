07a66d07757e7c767370ddbc19a330104516815d
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-08-07 18:09:45 -0300

    render message

--- src/routes/file.tsx
+++ src/routes/file.tsx
@@ -3,14 +3,16 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/file")({
   component: RouteComponent,
   loader: async () => {
-    const json = await Bun.file("src/data/file.json", {
+    const file = await Bun.file("src/data/file.json", {
       type: "application/json",
     }).text();
 
-    return { json };
+    return { json: JSON.parse(file) };
   },
 });
 
 function RouteComponent() {
-  return <div>Hello "/file"!</div>;
+  const { json } = Route.useLoaderData();
+
+  return <div>{JSON.stringify(json, null, 2)}</div>;
 }
